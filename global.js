5b59d0f9b33a6bb2edb94703de562922d0cd08af
Noely Gangello <noely.gangello@outlook.com>
2023-12-30 21:01:22 -0300

    Update global.js

--- global.js
+++ global.js
@@ -1,22 +1,26 @@
- 
 
+
+"use strict";
+/* == [ properties ]
+== == == == == == == == ==  */
 const 
-   _ = ( ...v ) => console.log( ...v ),
-   $ = v => document.querySelector( v ),
-   $$ = v => document.querySelectorAll( v )
+	_ = ( ...v ) => console.log( ...v ),
+	$ = v => document.querySelector( v ),
+	$$ = v => document.querySelectorAll( v )
 ;
+/* -- -- -- -- -- -- -- -- -- */
 
 addEventListener( "load", ev => {
-   let 
-      NavLinks = $$( "navlink" )
+	let 
+		NavLinks = $$( "navlink" )
    ;
  
    
-   NavLinks.forEach( nv => {
-      nv.addEventListener( "click", ev => {
-         window.open( nv.getAttribute( "to" ), "_blank" );
-      } );
-   } );
+	NavLinks.forEach( nv => {
+		nv.addEventListener( "click", ev => {
+			window.open( nv.getAttribute( "to" ), "_blank" );
+		} );
+	} );
 } );
 
 _( "oi!" );
